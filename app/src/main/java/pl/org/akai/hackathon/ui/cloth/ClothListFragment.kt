1efc5f91da2b15e6fcb92b3e39fb1ae354b80331
luncenok <idziejczak3@gmail.com>
2022-11-19 23:03:37 +0100

    Add Dropdown menu for sorting

--- app/src/main/java/pl/org/akai/hackathon/ui/cloth/ClothListFragment.kt
+++ app/src/main/java/pl/org/akai/hackathon/ui/cloth/ClothListFragment.kt
@@ -2,18 +2,11 @@ package pl.org.akai.hackathon.ui.cloth
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import pl.org.akai.hackathon.databinding.ListFragmentBinding
 import pl.org.akai.hackathon.ui.base.BaseFragment
-import pl.org.akai.hackathon.ui.base.ListLoadStateAdapter
 
 @AndroidEntryPoint
 class ClothListFragment : BaseFragment<ListFragmentBinding>(ListFragmentBinding::inflate) {
@@ -26,6 +19,7 @@ class ClothListFragment : BaseFragment<ListFragmentBinding>(ListFragmentBinding:
 		b.vm = vm
 
 		b.list.adapter = vm.adapter
+
 //		b.list.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
 		b.list.layoutManager = GridLayoutManager(context, 2)
 
